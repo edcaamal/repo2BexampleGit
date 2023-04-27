7e2c671a978e9ffaa7f7339ed12fcf3517dc6a40
Edgar David Caamal <edcaamal@uacam.mx>
2023-04-27 15:41:35 -0500

    Saludos inicial

--- Clase2BObjetos/src/clase2bobjetos/Clase2BObjetos.java
+++ Clase2BObjetos/src/clase2bobjetos/Clase2BObjetos.java
@@ -4,6 +4,9 @@ public class Clase2BObjetos {
 
     public static void main(String[] args) {
          System.out.println("Hola mundo GIT");
+         System.out.println("Saludos desde la UACAM");
+         System.out.println("El 2B te saludos ");
+         
     }
     
 }
