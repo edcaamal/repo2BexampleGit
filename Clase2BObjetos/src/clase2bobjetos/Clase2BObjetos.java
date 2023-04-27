df5eb49f1b6fc42817221dd027e22bbece125f51
Edgar David Caamal <edcaamal@uacam.mx>
2023-04-27 15:42:40 -0500

    Borrar linea

--- Clase2BObjetos/src/clase2bobjetos/Clase2BObjetos.java
+++ Clase2BObjetos/src/clase2bobjetos/Clase2BObjetos.java
@@ -3,7 +3,6 @@ package clase2bobjetos;
 public class Clase2BObjetos {
 
     public static void main(String[] args) {
-         System.out.println("Hola mundo GIT");
          System.out.println("Saludos desde la UACAM");
          System.out.println("El 2B te saludos ");
          
